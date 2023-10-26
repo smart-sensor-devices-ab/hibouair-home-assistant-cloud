b2406a12970b1e0dc0db55fdc8dea3bfc6099f9c
sheikh shuhad <shuhadz@hotmail.com>
2023-10-26 22:49:56 +0600

    readme card helper

---
+++ README.py
@@ -0,0 +1,26 @@
+add this lines to configuration.yaml
+sensor: 
+    - platform: hibouair
+    scan_interval: 30
+
+Sample Entities Card configuration
+type: entities
+entities:
+  - entity: sensor.hibouair_co2
+  - entity: sensor.hibouair_temperature
+  - entity: sensor.hibouair_pressure
+  - entity: sensor.hibouair_humidity
+  - entity: sensor.hibouair_voc
+  - entity: sensor.hibouair_light
+  - entity: sensor.hibouair_pm1
+  - entity: sensor.hibouair_pm25
+  - entity: sensor.hibouair_pm10
+  - entity: sensor.hibouair_last_update
+header:
+  type: picture
+  image: https://www.hibouair.com/blog/wp-content/uploads/2023/10/hibouair_banner.png
+  tap_action:
+    action: none
+  hold_action:
+    action: none
+
